06ab81b773740b2e3bc1f957ccdcecf4e713d6e1
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2020-10-16 22:58:49 +0100

    Add files via upload

---
+++ leonsworld/GUI/FadeOut.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeOut : MonoBehaviour
+{
+    // REFERENCE: https://forum.unity.com/threads/fading-in-out-gui-text-with-c-solved.380822/
+
+    // can ignore the update, it's just to make the coroutines get called for example
+    void Update()
+    {
+        StartCoroutine(FadeTextToZeroAlpha(2f, GetComponent<Text>()));
+    }
+
+    // This increases the transparency of the text over time i.e. Time.deltaTime which is
+    // divided by a value "t" (2f) to make it so the transparency gets reached in 2 seconds
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+}
---
+++ leonsworld/GUI/ParallaxBackground.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    // Move the background at a certain speed
+	public float speed = 0.5f;
+	
+	// Use this for initialization
+	void Start ()
+    {
+        // Change the sorting order of the sprite
+		GetComponent<Renderer>().sortingOrder = 5;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Change the offset of the texture in accordance to the speed and time pushing
+        // the speed forward
+		Vector2 offset = new Vector2(Time.time * speed, 0);
+		GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
