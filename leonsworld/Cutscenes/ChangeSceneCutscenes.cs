4f5df3a0aba299e555b1a0f03283cff7938c90cd
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2020-10-16 22:55:41 +0100

    Add files via upload

---
+++ leonsworld/Cutscenes/ChangeSceneCutscenes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LeonsWorld
+{
+    public class ChangeSceneCutscenes : MonoBehaviour
+    {
+        // This will be used to store the value entered in the inspector
+        public string sceneToChangeTo;
+        // This will be used to specify how long to wait until the scene will change
+        public int changeSceneAfterX;
+
+        // This is ran when the script first executes
+        void Start()
+        {
+            // Start the coroutine
+            StartCoroutine(LoadScene());
+        }
+
+        IEnumerator LoadScene()
+        {
+            // Wait X seconds before changing the scene
+            yield return new WaitForSeconds(changeSceneAfterX);
+
+            // Change to the scene specified in the inspector
+            SceneManager.LoadScene(sceneToChangeTo);
+        }
+    }
+}
---
+++ leonsworld/Cutscenes/ChangeSceneLevelTwo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace LeonsWorld
+{
+    public class ChangeSceneLevelTwo : MonoBehaviour
+    {
+        // Normal click sound 
+        public AudioSource normalClick;
+
+        // Negative sound so it lets the player know that they cannot select
+        // the level
+        public AudioSource secondLevelNegative;
+
+	    public void ChangeToScene (string sceneToChangeTo)
+        {
+            // If the level is unlocked in the PlayerPrefs then you can change to the second level
+            if (PlayerPrefs.GetInt("LevelUnlock", 0) == 3)
+            {
+                normalClick.Play();
+
+                StartCoroutine(ChangeSceneAfterHalfSecond(sceneToChangeTo));
+            }
+
+            if (PlayerPrefs.GetInt("LevelUnlock", 0) < 3)
+            {
+                // Play the negative sound
+                secondLevelNegative.Play();
+            }
+        }
+
+        // This will change the scene after half of a second
+        IEnumerator ChangeSceneAfterHalfSecond(string sceneToChangeTo)
+        {
+            yield return new WaitForSeconds(0.5f);
+            SceneManager.LoadScene(sceneToChangeTo);
+        }
+    }
+}
