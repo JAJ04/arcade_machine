55bafd750ab9b189731fecdea7212b29e60cc9be
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2020-10-16 22:54:22 +0100

    Create BulletCollisions.cs

---
+++ leonsworld/Collisions/BulletCollisions.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+namespace LeonsWorld
+{
+    public class BulletCollisions : MonoBehaviour 
+    {
+        // Sound properties to play when an
+        // event happens
+	    public AudioSource snowballDestroyed;
+	    public AudioSource bulletDestroyed;
+
+	    void OnTriggerEnter(Collider other)
+	    {
+            // Destroy the bullet on
+            // any object that this script
+            // is attached to
+		    if (other.tag == "PlayerBullet")
+		    {
+			    Destroy (other);
+		    }
+		
+            // Play the bullet destroyed sound
+            // and destroy the nerf bullet
+		    if (other.tag == "PlayerBulletNerf")
+		    {
+			    Destroy (other);
+			    bulletDestroyed.Play();
+		    }
+		
+            // Play the snowballDestroyed sound
+            // if the tag is "Bullet"
+		    if (other.tag == "Bullet")
+		    {
+			    snowballDestroyed.Play();
+		    }
+	    }	
+    }
+}
