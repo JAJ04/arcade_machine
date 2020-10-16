6f2bc53bba1e22e45d93c19bfce8d9fd6ffef405
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2020-10-16 23:02:46 +0100

    Add files via upload

---
+++ leonsworld/Prefabs/BulletScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LeonsWorld
+{
+    public class BulletScript : MonoBehaviour 
+    {	
+        // If the bullet hits any of the tags do something and destroy the
+        // game object attached to the tag no matter what
+	    void OnTriggerEnter(Collider other)
+	    {
+		    if(other.gameObject.tag == "Enemy")
+		    {
+                // Fire these methods if this condition above is true and do it for the below tags too
+			    Destroy(gameObject);
+			    other.gameObject.SendMessage("EnemyDamaged", GameManager.bulletDamage, SendMessageOptions.DontRequireReceiver);
+			    other.gameObject.SendMessage("TakenDamage", SendMessageOptions.DontRequireReceiver);
+		    }
+		
+            // Same as the below "Wall" tag but you will send a message to the "BossDamaged" function
+		    if(other.gameObject.tag == "SnowballBoss")
+		    {
+			    Destroy(gameObject);
+			    other.gameObject.SendMessage("BossDamaged", GameManager.bulletDamage, SendMessageOptions.DontRequireReceiver);
+		    }
+		
+            // If a bullet hits a wall then destroy "this" bullet/game object
+		    if(other.gameObject.tag == "Wall")
+		    {
+			    Destroy(gameObject);
+		    }
+	    }
+	
+        // Destroy the bullet after 0.6f seconds automatically
+	    void FixedUpdate()
+	    {
+		    Destroy(gameObject, 0.6f);
+	    }
+    }
+}
---
+++ leonsworld/Prefabs/EnemyBullet.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LeonsWorld
+{
+    public class EnemyBullet : MonoBehaviour 
+    {
+        // Bullet speed
+        private float speed;
+
+        // Direction of bullet
+        private Vector2 _direction; 
+
+        // Check to see if bullet can be shot
+        private bool isReady; 
+	
+        // This is used to play a sound when the bullet is destroyed
+	    public AudioSource bulletDestroyed;
+
+	    void Awake()
+	    {
+            // This is used to prevent overlapping between bullets
+            GetComponent<SpriteRenderer>().sortingOrder = Random.Range(9, 32766);
+            // Speed at which the bullet travels
+            speed = 5f;
+		    isReady = false;
+	    }
+
+	    void OnTriggerEnter(Collider other)
+	    {
+            // Destroy this game object if it hits any of these tags and possibly play a sound
+		    if(other.gameObject.tag == "Wall" && gameObject != null)
+		    {
+			    Destroy(gameObject);
+		    }
+		
+		    if(other.gameObject.tag == "PlayerBullet" && gameObject != null)
+		    {
+			    bulletDestroyed.Play();
+			    Destroy(gameObject, 0.1f);
+		    }
+		
+		    if(other.gameObject.tag == "PlayerBulletNerf" && gameObject != null)
+		    {
+			    bulletDestroyed.Play();
+			    Destroy(gameObject, 0.1f);
+		    }
+		
+		    if(other.gameObject.tag == "CanBullet" && gameObject != null)
+		    {
+			    Destroy(gameObject);
+		    }
+		
+		    if(other.gameObject.tag == "ThrowerBullet" && gameObject != null)
+		    {
+			    Destroy(gameObject);
+		    }
+	    }
+
+	    // where will the bullet shoot?
+	    public void SetDirection(Vector2 direction)
+	    {
+		    // direction is normalized meaning give it a length (mag) of 1
+		    _direction = direction.normalized;
+
+            // Set to true so the bullet can be hit
+            isReady = true; 
+	    }
+	
+	    // Update is called once per frame
+	    void Update ()
+	    {
+		    if (isReady)
+            {
+                // Rotate the bullet -45 degrees each update time
+			    transform.Rotate (Vector3.forward, -45 * Time.deltaTime);
+			
+			    // Get the bullet current position
+			    Vector2 position = transform.position;
+
+			    // Move the bullet to a new position
+			    position += _direction * speed * Time.deltaTime;
+
+			    // Update bullet position
+			    transform.position = position;
+
+			    // Removing the bullet
+
+			    // Bottom left of screen
+			    Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+			    // Top right of screen
+			    Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+
+			    // Destroy bullet if outside of these bounds
+			    if ((transform.position.x < min.x) || (transform.position.x > max.x) || (transform.position.y < min.x) || (transform.position.y > max.y))
+			    {
+				    StartCoroutine ("WaitAndDestroy");
+			    }
+		    }
+	    }
+
+	    IEnumerator WaitAndDestroy()
+	    {
+            // Wait 10 seconds before destroying the bullet
+		    yield return new WaitForSeconds(10);
+		    Destroy (gameObject);
+	    }
+    }
+}
