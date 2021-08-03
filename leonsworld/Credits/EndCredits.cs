9b9cce5ecbe1202b56e11790058b07b064c64ea0
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-03 18:30:33 +0100

    Update EndCredits.cs

--- leonsworld/Credits/EndCredits.cs
+++ leonsworld/Credits/EndCredits.cs
@@ -7,28 +7,27 @@ namespace LeonsWorld
     public class EndCredits : MonoBehaviour
     {
         // Holds the property for the camera
-	    public GameObject mainCamera;
+	public GameObject mainCamera;
 
         // The camera will move down at this speed
-	    public int speed = 1;
+	public int speed = 1;
 
         // This is the variable used to change the level
-	    public string level;
+	public string level;
 	
-	    // Update is called once per frame
-	    void Update () {
-
-            // Move the camera down every update at a certain speed
-            mainCamera.transform.Translate(Vector3.down * Time.deltaTime * speed);
-		
-		    StartCoroutine(waitFor());
-	    }
+	// Update is called once per frame
+	void Update () 
+	{
+             // Move the camera down every update at a certain speed
+             mainCamera.transform.Translate(Vector3.down * Time.deltaTime * speed);
+	     StartCoroutine(waitFor());
+	}
 
         // Load the specified level after 37 seconds
         IEnumerator waitFor()
-	    {
-		    yield return new WaitForSeconds(37);
-		    SceneManager.LoadScene(level);
-	    }
+	{
+             yield return new WaitForSeconds(37);
+             SceneManager.LoadScene(level);
+	}
     }
 }
