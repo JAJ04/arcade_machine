f55c0b61a67aa653d61fc7d3909d6bc1692beeb6
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:34:39 +0100

    Update Checker.cs

--- leonsworld/Checker.cs
+++ leonsworld/Checker.cs
@@ -2,55 +2,57 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-namespace LeonsWorld
+namespace LeonsWorld 
 {
-    public class Checker : MonoBehaviour
+    public class Checker: MonoBehaviour 
     {
         // Get properties of the game manager (reference)
         public GameManager gameManager;
 
         // Get a reference to the "angel"/"death" sound
         public AudioSource angel;
-	
-	    void Update()
-	    {
+
+        void Update()
+	{
             // If the game manager is deleted in other words
-		    if(gameManager == null)
-		    {
-			    StartCoroutine("WaitForSeconds");
-		    }
-	    }
-	
-	    // Update is called once per frame
-	    void RestartScene ()
+            if (gameManager == null) 
 	    {
+                StartCoroutine("WaitForSeconds");
+            }
+        }
+
+        // Update is called once per frame
+        void RestartScene() 
+	{
             // Reload the scene
-		    if(gameManager == null)
-		    {
-			    StartCoroutine("WaitForSeconds");
-			    SceneManager.LoadScene(SceneManager.GetActiveScene().ToString());
-			    Time.timeScale = 1f;
-		    }
-	    }
-	
-        // After a certain amount of seconds this will return
-	    public static class CoroutineUtil
+            if (gameManager == null) 
 	    {
-		    public static IEnumerator WaitForRealSeconds(float time)
-		    {
-			    float start = Time.realtimeSinceStartup;
-			    while (Time.realtimeSinceStartup < start + time)
-			    {
-				    yield return null;
-			    }
-		    }
-	    }
+                StartCoroutine("WaitForSeconds");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().ToString());
+                Time.timeScale = 1 f;
+            }
+        }
+
+        // After a certain amount of seconds this will return
+        public static class CoroutineUtil 
+	{
+            public static IEnumerator WaitForRealSeconds(float time) 
+	    {
+                float start = Time.realtimeSinceStartup;
+		    
+                while (Time.realtimeSinceStartup < start + time) 
+		{
+                    yield
+                    return null;
+                }
+            }
+        }
 
         // Reloads the scene after 5 seconds
-	    IEnumerator WaitForSeconds()
-	    {
-		    yield return StartCoroutine(CoroutineUtil.WaitForRealSeconds(5));
-		    RestartScene ();
-	    }
+        IEnumerator WaitForSeconds() 
+	{
+            yield return StartCoroutine(CoroutineUtil.WaitForRealSeconds(5));
+            RestartScene();
+        }
     }
 }
