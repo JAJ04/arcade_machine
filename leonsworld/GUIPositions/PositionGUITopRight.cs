a23974d444b03f38dbeefb41d0046d5f608a2f8a
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 04:00:49 +0100

    Update PositionGUITopRight.cs

--- leonsworld/GUIPositions/PositionGUITopRight.cs
+++ leonsworld/GUIPositions/PositionGUITopRight.cs
@@ -1,15 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PositionGUITopRight : MonoBehaviour
 {
-     // Use this for initialization
-     void Update ()
-	 {
+    // Use this for initialization
+    void Update() 
+    {
         // Keeps the DOOM face clamped to the screen despite the resolution
-         Vector3 pos = Camera.main.WorldToViewportPoint (transform.position);
-         pos.x = Mathf.Clamp(pos.x, 0.97f, 0.97f);
-         pos.y = Mathf.Clamp(pos.y, 0.95f, 0.95f);
-         transform.position = Camera.main.ViewportToWorldPoint(pos);
-	 }
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0.97 f, 0.97 f);
+        pos.y = Mathf.Clamp(pos.y, 0.95 f, 0.95 f);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
 }
