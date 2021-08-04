c7e668b84eca84bd75bc0099846ae9ef2f8aaa6c
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:48:48 +0100

    Update DestroyExplosion.cs

--- martianattack/DestroyExplosion.cs
+++ martianattack/DestroyExplosion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyExplosion: MonoBehaviour
+public class DestroyExplosion : MonoBehaviour
 {
     // Use this for initialization
     void Start() 
