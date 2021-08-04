5d9a745158ea495c8e21027f61121e51e7afa60c
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:16:35 +0100

    Update DestroyExplosion.cs

--- martianattack/DestroyExplosion.cs
+++ martianattack/DestroyExplosion.cs
@@ -6,7 +6,7 @@ public class DestroyExplosion : MonoBehaviour
 {
 	// Use this for initialization
 	void Start ()
-    {
-        Destroy(gameObject, 5f);
+    	{
+        	Destroy(gameObject, 5f);
 	}
 }
