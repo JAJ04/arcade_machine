17d0efdf514e2fe4bd100e054f26d68dddd3a45f
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 04:01:18 +0100

    Update PositionGUIWinFace.cs

--- leonsworld/GUIPositions/PositionGUIWinFace.cs
+++ leonsworld/GUIPositions/PositionGUIWinFace.cs
@@ -1,15 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class PositionGUIWinFace : MonoBehaviour
+public class PositionGUIWinFace: MonoBehaviour 
 {
-     // Use this for initialization
-     void Update ()
-	 {
+    // Use this for initialization
+    void Update() 
+    {
         // Keeps the DOOM face clamped to the screen despite the resolution
-         Vector3 pos = Camera.main.WorldToViewportPoint (transform.position);
-         pos.x = Mathf.Clamp(pos.x, 0.95f, 0.95f);
-         pos.y = Mathf.Clamp(pos.y, 0.92f, 0.92f);
-         transform.position = Camera.main.ViewportToWorldPoint(pos);
-	 }
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0.95 f, 0.95 f);
+        pos.y = Mathf.Clamp(pos.y, 0.92 f, 0.92 f);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
 }
