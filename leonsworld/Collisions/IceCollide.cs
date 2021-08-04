415d9fbd90105b306e7aa73808e1d5d20553bd0d
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 04:10:36 +0100

    Update IceCollide.cs

--- leonsworld/Collisions/IceCollide.cs
+++ leonsworld/Collisions/IceCollide.cs
@@ -1,36 +1,37 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 namespace LeonsWorld
 {
-    public class IceCollide : MonoBehaviour {
-	
+    public class IceCollide : MonoBehaviour 
+    {
+
         // Gets the Player properties
-	public PlayerControl playerControl;
-	
+        public PlayerControl playerControl;
+
         // This is used to play the slip sound
-	public AudioSource slip;
+        public AudioSource slip;
 
         // This script is attached to the
         // slippy ice game objects
-	void OnTriggerEnter(Collider other)
+        void OnTriggerEnter(Collider other) 
 	{
-                    // If the tag is the "Player" then
-                    // increase the speed of the player slightly
-		    if(other.tag == "Player")
-		    {
-			    slip.Play();
-			    playerControl.walkSpeed = 20f;
-		    }
-	    }
-	
-	    void OnTriggerExit(Collider other)
+            // If the tag is the "Player" then
+            // increase the speed of the player slightly
+            if (other.tag == "Player") 
 	    {
-		    if(other.tag == "Player")
-		    {
-                            // Return the player walk speed to normal
-			    playerControl.walkSpeed = 5f;
-		    }
-	    }
+                slip.Play();
+                playerControl.walkSpeed = 20 f;
+            }
+        }
+
+        void OnTriggerExit(Collider other) 
+	{
+            if (other.tag == "Player") 
+	    {
+                // Return the player walk speed to normal
+                playerControl.walkSpeed = 5 f;
+            }
+        }
     }
 }
