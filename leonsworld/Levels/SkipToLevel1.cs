a156d8c577ebf19d9ddd5f4b02be0c48ed1c788b
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:58:37 +0100

    Update SkipToLevel1.cs

--- leonsworld/Levels/SkipToLevel1.cs
+++ leonsworld/Levels/SkipToLevel1.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SkipToLevel1 : MonoBehaviour
+public class SkipToLevel1: MonoBehaviour 
 {
     // Reference to the sound to play
     public AudioSource backSound;
@@ -12,26 +12,26 @@ public class SkipToLevel1 : MonoBehaviour
     bool playSoundOnce = true;
 
     // Update is called once per frame
-    void Update ()
+    void Update() 
     {
         // Change to level 1 when this is attached to a game object during the cutscenes for level 1
-		if(Input.GetKeyDown(KeyCode.LeftAlt))
-        {
+        if (Input.GetKeyDown(KeyCode.LeftAlt)) 
+	{
             StartCoroutine(GoBackToLevel1());
         }
-	}
+    }
 
     // Play a sound and go to the quit menu
-    IEnumerator GoBackToLevel1()
+    IEnumerator GoBackToLevel1() 
     {
         // Play the sound only once
         if (playSoundOnce)
-        {
+	{
             backSound.Play();
             playSoundOnce = false;
         }
 
-        yield return new WaitForSeconds(0.25f);
+        yield return new WaitForSeconds(0.25 f);
 
         SceneManager.LoadScene("level1");
     }
