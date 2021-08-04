2825c4740cb463bd3b86834f829e2fd9f41d92c0
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:13:12 +0100

    Update ChangeBossColor.cs

--- martianattack/Scoreboard/ChangeBossColor.cs
+++ martianattack/Scoreboard/ChangeBossColor.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace MartianAttack
+namespace MartianAttack 
 {
-    public class ChangeBossColor : MonoBehaviour
+    public class ChangeBossColor: MonoBehaviour 
     {
         // Used to slow down the changing of the sprite color
         private float _timer = 0;
-	
-	    // Update is called once per frame
-	    void Update ()
-        {
+
+        // Update is called once per frame
+        void Update() 
+	{
             // Keep changing the sprite color after 2 seconds
             _timer += Time.deltaTime;
 
-            if (_timer > 1f)
-            {
+            if (_timer > 1 f) 
+	    {
                 // Keep changing colors over and over again
-                gameObject.GetComponent<Image>().color = Random.ColorHSV();
-                _timer = 0f;
+                gameObject.GetComponent < Image > ().color = Random.ColorHSV();
+                _timer = 0 f;
             }
         }
     }
 }
-
