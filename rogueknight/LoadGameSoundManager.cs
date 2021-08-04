55843fb51ddcf252eba78ed9bbafb88595859ce4
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:05:15 +0100

    Update LoadGameSoundManager.cs

--- rogueknight/LoadGameSoundManager.cs
+++ rogueknight/LoadGameSoundManager.cs
@@ -1,29 +1,29 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-namespace RogueKnight
-{	
-	public class LoadGameSoundManager : MonoBehaviour 
-	{
+namespace RogueKnight 
+{
+    public class LoadGameSoundManager: MonoBehaviour 
+    {
         // These are game objects that will hold the associated scripts
-		public GameObject gameManager;
-		public GameObject soundManager;		
+        public GameObject gameManager;
+        public GameObject soundManager;
 
-		void Awake ()
-		{
+        void Awake() 
+	{
             // Checks to see if a GameManager has been assigned to a static variable
-            if (GameManager.gmInstance == null)
-            {
+            if (GameManager.gmInstance == null) 
+	    {
                 // If not then instantiate the gameManager prefab
                 Instantiate(gameManager);
             }
 
             // Checks to see if a SoundManager has been assigned to a static variable
-            if (SoundManager.instance == null)
-            {
+            if (SoundManager.instance == null) 
+	    {
                 // If not then instantiate the soundManager prefab
                 Instantiate(soundManager);
             }
-		}
-	}
+        }
+    }
 }
