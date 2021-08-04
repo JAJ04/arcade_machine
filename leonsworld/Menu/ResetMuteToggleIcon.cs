1fadc0d5d8b2981d1623b61f0f8b0288e02429ea
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:56:08 +0100

    Update ResetMuteToggleIcon.cs

--- leonsworld/Menu/ResetMuteToggleIcon.cs
+++ leonsworld/Menu/ResetMuteToggleIcon.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResetMuteToggleIcon : MonoBehaviour
-{	
-	// Update is called once per frame
+public class ResetMuteToggleIcon : MonoBehaviour 
+{
+    // Update is called once per frame
 
     // Allows the mute icon to be used properly outside of the Leon's World
     // gameplay scenes again
-	void Update ()
+    void Update() 
     {
         MuteSystem.muteIconToggle = true;
     }
