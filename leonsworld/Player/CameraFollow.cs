eb0af6cb2a4aa4142867b472419b383f7b4e5fe1
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:37:18 +0100

    Update CameraFollow.cs

--- leonsworld/Player/CameraFollow.cs
+++ leonsworld/Player/CameraFollow.cs
@@ -1,27 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
- 
-namespace LeonsWorld
+
+namespace LeonsWorld 
 {
-    public class CameraFollow : MonoBehaviour
+    public class CameraFollow: MonoBehaviour 
     {
         // Gets player position
-	    public Transform Player;
+        public Transform Player;
 
         // Vector to keep the camera on the target of the player
-	    private Vector3 Direction;
+        private Vector3 Direction;
 
-	    // Use this for initialization
-	    // This subtracts the world position and the player position to get the camera position
-	    void Start ()
-	    {
-			Direction = transform.position - Player.transform.position;
-	    }
+        // Use this for initialization
+        // This subtracts the world position and the player position to get the camera position
+        void Start() 
+	{
+            Direction = transform.position - Player.transform.position;
+        }
 
-	    // This changes the world position by getting the player direction and adding where the camera position is
-	    void LateUpdate ()
-	    {
-			transform.position = Player.transform.position + Direction;
-	    }
+        // This changes the world position by getting the player direction and adding where the camera position is
+        void LateUpdate()
+	{
+            transform.position = Player.transform.position + Direction;
+        }
     }
 }
