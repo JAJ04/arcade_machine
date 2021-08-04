a75ccf0701b86dfdfb06ffc9ca72ef17ff4d0aac
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:57:56 +0100

    Update SaveSpot.cs

--- leonsworld/Levels/SaveSpot.cs
+++ leonsworld/Levels/SaveSpot.cs
@@ -1,31 +1,31 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-namespace LeonsWorld
+namespace LeonsWorld 
 {
-    public class SaveSpot : MonoBehaviour
+    public class SaveSpot : MonoBehaviour 
     {
         // Reference to the game manager
-	    public GameManager gm;
+        public GameManager gm;
         // Gets the scene name and stores it into a string
-	    public string level;
+        public string level;
 
         // If the player enters the save spot then go to the next scene and save
-	    void OnTriggerEnter(Collider other)
+        void OnTriggerEnter(Collider other) 
+	{
+            if (other.tag == "Player") 
 	    {
-		    if(other.tag == "Player")
-		    {
                 Time.timeScale = 1;
-			    gm.SaveGame();
-			    SceneManager.LoadScene(level);
+                gm.SaveGame();
+                SceneManager.LoadScene(level);
 
                 // Set the LevelUnlock integers depending on what scenes are loaded
-                if(SceneManager.GetActiveScene() == SceneManager.GetSceneByName("level1"))
-                {
+                if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("level1")) 
+		{
                     PlayerPrefs.SetInt("LevelUnlock", 2);
                 }
             }
-	    }
+        }
     }
 }
