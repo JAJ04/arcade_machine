78a089cf721e88531a474f870a0b9a62b29d22ff
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:17:25 +0100

    Update EnemyBullet.cs

--- martianattack/EnemyBullet.cs
+++ martianattack/EnemyBullet.cs
@@ -1,29 +1,29 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MartianAttack
+namespace MartianAttack 
 {
-    public class EnemyBullet : MonoBehaviour
+    public class EnemyBullet: MonoBehaviour
     {
         // Sets the speed of the bullet
         [SerializeField]
-        private float _speed = 15.0f;
-	
-	    // Update is called once per frame
-	    void Update ()
-        {
+        private float _speed = 15.0 f;
+
+        // Update is called once per frame
+        void Update() 
+	{
             // Move down at 15 speed using Time.deltaTime
             transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
             // If the bullet goes off the screen then destroy the bullet
             // to save memory
-            if (transform.position.y < -6.07f)
-            {
+            if (transform.position.y < -6.07 f) 
+	    {
                 // Destroy the left-over parent game objects to prevent
                 // bloated memory
-                if(transform.parent != null)
-                {
+                if (transform.parent != null) 
+		{
                     Destroy(transform.parent.gameObject);
                 }
 
