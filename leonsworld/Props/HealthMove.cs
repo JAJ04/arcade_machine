89d55d92232fb9b4cb92623d6c9de10c04da65ad
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:35:06 +0100

    Update HealthMove.cs

--- leonsworld/Props/HealthMove.cs
+++ leonsworld/Props/HealthMove.cs
@@ -1,37 +1,38 @@
 using UnityEngine;
 using System.Collections;
 
-public class HealthMove : MonoBehaviour 
+public class HealthMove: MonoBehaviour
 {
     // This will start the health sprite at one destination and will move it to another destination
     public Transform originSpot;
     public Transform destinationSpot;
 
     // Move the sprite at a certain speed
-	public float speed;
-    
+    public float speed;
+
     // This is a bool that allows the health kit to move in a different direction
-	private bool switchBool = false;
+    private bool switchBool = false;
 
     // Needs to be "FixedUpdate()" is Time.timeScale will pause this
-    void FixedUpdate()
+    void FixedUpdate() 
+    {
+        if (transform.position == destinationSpot.position) 
 	{
-		if (transform.position == destinationSpot.position) 
-		{
             switchBool = true;
-		} 
-		if (transform.position == originSpot.position) 
-		{
-			switchBool = false;
-		}
+        }
+	
+        if (transform.position == originSpot.position) 
+	{
+            switchBool = false;
+        }
 
-		if (switchBool) 
-		{
-			transform.position = Vector3.MoveTowards (transform.position, originSpot.position, speed);
-		}
-		else
-		{
-			transform.position = Vector3.MoveTowards (transform.position, destinationSpot.position, speed);
-		}
-	}
+        if (switchBool) 
+	{
+            transform.position = Vector3.MoveTowards(transform.position, originSpot.position, speed);
+        } 
+	else 
+	{
+            transform.position = Vector3.MoveTowards(transform.position, destinationSpot.position, speed);
+        }
+    }
 }
