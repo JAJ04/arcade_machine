d3e23454e8e1bf6380614980ae9e37701685540c
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 04:00:29 +0100

    Update PositionGUITopLeft.cs

--- leonsworld/GUIPositions/PositionGUITopLeft.cs
+++ leonsworld/GUIPositions/PositionGUITopLeft.cs
@@ -1,15 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class PositionGUITopLeft : MonoBehaviour
+public class PositionGUITopLeft : MonoBehaviour 
 {
-     // Use this for initialization
-     void Update ()
-	 {
+    // Use this for initialization
+    void Update()
+    {
         // Keeps the player data text clamped to the screen despite resolution
-         Vector3 pos = Camera.main.WorldToViewportPoint (transform.position);
-         pos.x = Mathf.Clamp(pos.x, 0.028f, 0.028f);
-         pos.y = Mathf.Clamp01(pos.y);
-         transform.position = Camera.main.ViewportToWorldPoint(pos);
-	 }
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0.028 f, 0.028 f);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
 }
