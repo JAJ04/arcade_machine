642e9f9fd8f15cfcd6e78cf8967bbac51872816c
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 03:17:54 +0100

    Update ExplosionSpriteEffect.cs

--- martianattack/ExplosionSpriteEffect.cs
+++ martianattack/ExplosionSpriteEffect.cs
@@ -1,27 +1,27 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MartianAttack
+namespace MartianAttack 
 {
-    public class ExplosionSpriteEffect : MonoBehaviour
+    public class ExplosionSpriteEffect: MonoBehaviour 
     {
         // Reference to the audio source
         private AudioSource _audioSource;
 
         // Use this for initialization
-	    void Start ()
-        {
+        void Start() 
+	{
             // Get audio source on this object
-            _audioSource = GetComponent<AudioSource>();
+            _audioSource = GetComponent <AudioSource>();
 
             _audioSource.Play();
 
-            if(gameObject.activeSelf == true)
-            {
+            if (gameObject.activeSelf == true) 
+	    {
                 // To save memory, make sure to delete the sprite after 3 seconds
-                Destroy(gameObject, 3f);
+                Destroy(gameObject, 3 f);
             }
-	    }
+        }
     }
 }
