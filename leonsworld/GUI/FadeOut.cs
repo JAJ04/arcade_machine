c9db80561d79db4129cd8e04afc5eb282927fe5b
JAJ04 <73000395+JAJ04@users.noreply.github.com>
2021-08-04 04:03:58 +0100

    Update FadeOut.cs

--- leonsworld/GUI/FadeOut.cs
+++ leonsworld/GUI/FadeOut.cs
@@ -17,6 +17,7 @@ public class FadeOut : MonoBehaviour
     public IEnumerator FadeTextToZeroAlpha(float t, Text i)
     {
         i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        
         while (i.color.a > 0.0f)
         {
             i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
